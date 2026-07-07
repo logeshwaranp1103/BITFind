b4261cccd50f1616be8881feb32ba312215c25cd
logeshwaranp1103 <logeshwaranpalanic7@gmail.com>
2026-07-07 16:12:09 +0530

    ch2

--- src/App.jsx
+++ src/App.jsx
@@ -37,7 +37,7 @@ function App() {
   }
 
   return (
-    <Router>
+    <Router basename={import.meta.env.BASE_URL}>
       <div className="app-container">
         {isLoggedIn && <Navbar onLogout={handleLogout} />}
         <Routes>
